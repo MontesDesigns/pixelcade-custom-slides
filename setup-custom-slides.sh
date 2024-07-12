b9643669dd143c522367f9f7fa9b7118186ec333
MontesDesigns <montesdesigns09@gmail.com>
2024-07-12 12:28:44 -0700

    Update setup-custom-slides.sh

--- setup-custom-slides.sh
+++ setup-custom-slides.sh
@@ -5,8 +5,8 @@ INSTALLPATH=$HOME"/"
 
 #Add custom slides file to startup
 echo "${yellow}Adding Custom Slides to Startup via pixelcade-custom-slides.service...${white}"
-wget -O ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/autostart-pixelcade-custom-slides.sh?token=GHSAT0AAAAAACUQMYVJTTGJJ4UULJEQDL2IZURQIVA
-wget -O ${INSTALLPATH}pixelcade/system/pixelcade-custom-slides.service https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/pixelcade-custom-slides.service?token=GHSAT0AAAAAACUQMYVJCZDUE5Q6ASI5NH3IZURQMTQ
+wget -O ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/autostart-pixelcade-custom-slides.sh
+wget -O ${INSTALLPATH}pixelcade/system/pixelcade-custom-slides.service https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/pixelcade-custom-slides.service
 #sudo chmod +x ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh # TO DO need to replace this
 sudo cp ${INSTALLPATH}pixelcade/system/pixelcade-custom-slides.service /etc/systemd/system/pixelcade-custom-slides.service
 
