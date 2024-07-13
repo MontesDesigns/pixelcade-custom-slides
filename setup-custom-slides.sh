afc97d286264a5a5829e4c5a15ef67998c1fd968
MontesDesigns <montesdesigns09@gmail.com>
2024-07-13 12:26:37 -0700

    Update setup-custom-slides.sh

--- setup-custom-slides.sh
+++ setup-custom-slides.sh
@@ -4,9 +4,32 @@
 INSTALLPATH=$HOME"/"
 
 #Add custom slides file to startup
-echo "${yellow}Adding Custom Slides to Startup via pixelcade-custom-slides.service...${white}"
-cd ${INSTALLPATH}pixelcade/system && rm autostart-pixelcade-custom-slides.sh && rm pixelcade-custom-slides.service
+echo "${yellow}Adding Custom Slides to Pixelcade folder...${white}"
+
+#Remove the old file if found in the pixelcade/system folder
+cd ${INSTALLPATH}pixelcade/system && rm autostart-pixelcade-custom-slides.sh &
+
+#Download the new file
 wget -O ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/autostart-pixelcade-custom-slides.sh
-wget -O ${INSTALLPATH}pixelcade/system/pixelcade-custom-slides.service https://raw.githubusercontent.com/MontesDesigns/pixelcade-custom-slides/main/system/pixelcade-custom-slides.service
-sudo chmod +x ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh # TO DO need to replace this
-sudo cp ${INSTALLPATH}pixelcade/system/pixelcade-custom-slides.service /etc/systemd/system/pixelcade-custom-slides.service
+
+#Make sure it has execute permissions
+sudo chmod +x ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh
+echo "${yellow}Done adding Custom Slides to Pixelcade folder...${white}"
+
+# Check if arguments are provided
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 ${INSTALLPATH}pixelcade/system/autostart-pixelcade-custom-slides.sh @reboot"
+    echo "Example: $0 /home/user/myscript.sh '* * * * *'"
+    exit 1
+fi
+
+# Assign arguments to variables
+SCRIPT_PATH="$1"
+CRON_SCHEDULE="$2"
+
+# Add the cron job to the crontab
+(crontab -l ; echo "$CRON_SCHEDULE $SCRIPT_PATH >/dev/null 2>&1") | sort - | uniq - | crontab -
+
+echo "Cron job added:"
+echo "$CRON_SCHEDULE $SCRIPT_PATH"
+echo "reboot pi to test Cron Job"
