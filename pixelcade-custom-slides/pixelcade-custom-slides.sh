22c0f85863118f5fdf5a30cb6bf872fc5dd7719a
MontesDesigns <montesdesigns09@gmail.com>
2024-07-14 05:43:26 -0700

    Update pixelcade-custom-slides.sh

--- pixelcade-custom-slides/pixelcade-custom-slides.sh
+++ pixelcade-custom-slides/pixelcade-custom-slides.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Script logic
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+white=`tput setaf 7`
+reset=`tput sgr0`
+NEWLINE=$'\n'
+
 # Pause for effect
 sleep 10
 
