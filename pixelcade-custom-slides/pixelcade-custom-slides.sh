cea89b2fb3e5e561658a11209fa3f7df716e3d96
MontesDesigns <montesdesigns09@gmail.com>
2024-07-14 06:32:43 -0700

    Update pixelcade-custom-slides.sh

--- pixelcade-custom-slides/pixelcade-custom-slides.sh
+++ pixelcade-custom-slides/pixelcade-custom-slides.sh
@@ -12,6 +12,7 @@ reset=`tput sgr0`
 NEWLINE=$'\n'
 
 # Pause for effect
+echo "${yellow}Wait for Pixelcade boot up.${white}"  # Display specified seconds before the next iteration
 sleep 15
 
 # Check if pixelcade.service is running
